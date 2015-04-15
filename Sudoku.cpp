c9da53e1a4ac57ff40d1fc49d50ecd72c47bd5f4
F74034059 <f74034059@mail.nuku.edu.tw>
2015-04-15 20:24:41 +0800

    F74034059

---
+++ Sudoku.cpp
@@ -0,0 +1,97 @@
+#include<string>
+#include<vector>
+#include<cstdlib>
+#include<ctime>
+#include"Sudoku"
+using namespace std;         
+
+void Sudoku::GiveQuestion(){
+int q[12][12]={4,2,6,8,7,3,9,5,1,-1,-1,-1,
+               0,0,3,9,5,0,6,0,4,-1,-1,-1,
+               9,0,1,6,2,4,8,0,0,-1,-1,-1,
+               -1,-1,-1,1,3,2,0,8,7,9,5,6,
+               -1,-1,-1,0,8,0,1,9,0,4,2,0,
+               -1,-1,-1,4,9,6,2,3,0,8,7,1,
+               1,0,0,0,4,0,-1,-1,-1,6,9,5,
+               0,0,4,0,6,0,-1,-1,-1,1,3,7,
+               6,9,5,0,1,7,-1,-1,-1,2,8,4,
+               3,1,2,-1,-1,-1,7,4,0,5,0,9,
+               7,4,8,-1,-1,-1,0,6,9,3,0,2,
+               0,6,0,-1,-1,-1,3,1,0,7,0,8,};
+srand(time(NULL));
+int level=rand()%3
+switch(level){
+    case 0 
+    int x1=rand()%8;       
+    for(i=0; i<12; i++){
+       for(k=0; k<12; k++){
+          if(q[i][k]==0||q[i][k]==-1){
+          return a[i][k];
+          }
+          else if(1<=q[i][k]||q[i][k]<=(9-change)){
+          q[i][k]=q[i][k]+(change);
+          }
+          else if(q[i][k]>=(10-change)){
+          q[i][k]=q[i][k]-(9-change);
+          }
+       }
+    }
+    break;
+    
+    case 1
+    int x2=rand()%3;
+    int x22=rand()%3;
+    for(x2==x22){
+       x2=rand()%3;
+    }               
+    for(i=0; i<12; i++){ 
+             
+      
+           
+    case 11
+      
+      
+}             
+
+for(i=0; i<12; i++){
+    for(k=0; k<12; k++){
+        cout<<q[i][k];
+        }     
+    }
+    cout<<endl;                                                
+}              
+}
+
+ 
+void Sudoku::ReadIn(){
+int a[12][12];
+for(i=0; i<12; i++){
+    for(k=0; k<12; k++){
+        cin>>a[i][k];
+    }
+} 
+
+void Sudoku::Solve(){
+     int 
+     int checkC[144]; /*column*/
+     int checkR[144]; /*row*/
+     int checkB[144]; /*block*/
+     int nextC[144];
+     int nextR[144];
+     int nextB[144];
+     
+     for(i=0; i<144; i++) {
+         checkC[i]= i/12* 12 ;                       
+         checkR[i]= i% 12 ;                       
+         checkB[i]= ((i/12)/3)*36+ ((i%12)/3)*3 ;    
+	   }
+	   for(i=0; i<12; i++) {
+	      nextC[i]= i ;                             
+	      nextR[i]= i*12 ;                            
+	      nextB[i]= (i/3)*9+ (i%3) ;                
+     }
+
+         
+     
+}
+
---
+++ Sudoku.h
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>  
+#include<cstdlib>                                          
+using namespace std;
+
+class Sudoku{
+    void GiveQuestion();
+    void ReadIn();	
+    void Solve();
+};
+
